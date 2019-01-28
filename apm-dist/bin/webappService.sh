03979c5e277f781bd026018a0aaf58390697d081
zhangyunpeng <2305658511@qq.com>
2019-01-28 17:17:11 +0800

    整合配置中心

--- apm-dist/bin/webappService.sh
+++ apm-dist/bin/webappService.sh
@@ -18,7 +18,7 @@ _RUNJAVA=${JAVA_HOME}/bin/java
 [ -z "$JAVA_HOME" ] && _RUNJAVA=java
 
 eval exec "\"$_RUNJAVA\" ${JAVA_OPTS} -jar ${JAR_PATH}/skywalking-webapp.jar \
-         --spring.config.location=${JAR_PATH}/webapp.yml \
+         --env.configServerUrl=http://127.0.0.1/api/ \
          --logging.file=${LOG_FILE_LOCATION} \
         2>${WEBAPP_LOG_DIR}/webapp-console.log 1> /dev/null"
 
